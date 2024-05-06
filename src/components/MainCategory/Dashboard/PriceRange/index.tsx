0ff952da1b6b9bf3d5a78ee7089e9c3bb75d42a8
Hikmatjan <125250004+Hikmatjan@users.noreply.github.com>
2024-05-06 23:44:48 +0500

    first-commit

--- src/components/MainCategory/Dashboard/PriceRange/index.tsx
+++ src/components/MainCategory/Dashboard/PriceRange/index.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import { Slider } from "antd";
+const min = 0;
+const max = 1000;
 
 const PriceRange = () => {
-  const min = 0;
-  const max = 100;
-  const [price, setprice] = useState([]);
+  const [price, setprice] = useState<[number, number]>([0, 1000]);
   return (
     <div className="mt-4">
       <h1 className="font-bold">Price range</h1>
       <Slider
-        onChange={() => {
-          setprice;
-        }}
+        range
+        defaultValue={price}
+        min={0}
+        max={1000}
+        onChange={(e) => setprice(e)}
       />
-      Price: <span className="font-bold text-[#46A358]"> 0$ - 12$</span>
+      Price:{" "}
+      <span className="font-bold text-[#46A358] ">
+        {" "}
+        {price[0]}$ - {price[1]}$
+      </span>
       <button className="bg-[#46A358] flex rounded-md items-center justify-center text-base text-white mt-8 w-[90px] h-9 max-md:mt-3">
         Filter
       </button>
--- src/components/Page2/index.tsx
+++ src/components/Page2/index.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const Card2 = () => {
   return (
     <div className="w-full flex gap-2 mt-[150px]">
-      <div className="  flex justify-end w-[50%]  h-[250px] bg-[#FBFBFB]">
+      <div className="  relative flex justify-end w-[50%]  h-[250px] bg-[#FBFBFB]">
         <div className="w-[40%]">
           <img
-            src="https://www.figma.com/file/BeJ7F91dnIOSwVdvcRwMqn/image/7c597aa91ee5df5d1fd65d8184246c0c9918f920"
+            src="https://s3-alpha-sig.figma.com/img/9531/c63d/3f618aefdf1ccb10a5aa0e2d00f45d0b?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WdWtkl6PPmvwsiV2p4iZ7F1vxcrUDJw-ywVMbTcAamJmXH0BFwCnxJ7NksgGMGR9DJcqkM4HEW0wv9KJUb0-eXzQDLzGqNChtKx1x5gLkxn7NBeEgcQjk10Qu0Frys84Ql0vvdQmw6a8fkWH8aSCPEieUsRz1Kqjnwb2cKOWEmB7gjoTrJLZbSZUbte9kE-JggdDg8SWKa6u3sDdwda7OVFj94P9WNM2Y65gYT4FxWmFgWqL~AvcD5gUDrjaQZUzgACAFHMrEDWYkf3IpKtRJFMnqyVMDPq8RZCc5zeaQQ1RDUelnPDET7eHHwDsnG3fbMEvp9bm3r7HJi923TtDvg__"
             alt=""
-            className="w-[292px] h-[292px]"
+            className="w-[292px] h-[250px] absolute top-[-30px]"
           />
         </div>
         <div className="w-[60%] mt-8 ">
@@ -34,16 +34,16 @@ const Card2 = () => {
           </button>
         </div>
       </div>
-      <div className="  flex justify-end w-[50%]  h-[250px] bg-[#FBFBFB]">
+      <div className=" relative flex justify-end w-[50%]  h-[250px] bg-[#FBFBFB]">
         <div className="w-[40%]">
           <img
             src="https://www.figma.com/file/BeJ7F91dnIOSwVdvcRwMqn/image/7c597aa91ee5df5d1fd65d8184246c0c9918f920"
             alt=""
-            className="w-[292px] h-[292px]"
+            className="w-[292px] h-[250px]  absolute top-[-30px] "
           />
         </div>
         <div className="w-[60%] mt-8 ">
-          <h1 className=" ml-[150px] font-medium text-base max-lg:text-sm max-md:text-xs max-md:mt-4">
+          <h1 className="ml-[150px] font-medium text-base max-lg:text-sm max-md:text-xs max-md:mt-4">
             STYLING TRENDS
           </h1>
           <h1 className=" ml-[170px] font-medium text-base max-lg:text-sm max-md:text-xs max-md:mt-4">
