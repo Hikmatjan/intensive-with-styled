d292f1bf46dd5e23601f411f2d496acaa1a4eeb0
Hikmatjan <125250004+Hikmatjan@users.noreply.github.com>
2024-05-07 23:55:26 +0500

    first-commit

--- src/components/MainCategory/Dashboard/Sail/index.tsx
+++ src/components/MainCategory/Dashboard/Sail/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Sail = () => {
   return (
-    <div className=" mt-5 min-w-[310px] flex justify-center items-center gap-2 bg-[#F0F7F1]">
+    <div className=" mt-5 flex justify-center items-center gap-2 bg-[#F0F7F1]">
       <div className="mt-5 text-center text-base text-xl">
         <h1 className="text-5xl text-[#46A358]">Super Sale</h1>
         <p>UP TO 75% OFF</p>
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Fimages%2Fflower2.png?alt=media&token=905a94e2-1250-4e56-9dcb-d16bbb1a31ca"
+          src="https://s3-alpha-sig.figma.com/img/5e3c/6700/3f4b62655ec5dd62c1a46641197d5bbf?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZQLhxdFcGnXJ1voBpgdos7Lwz~VoNGANpv7ls7Zmjw6JEaiezAmkXszRqbVY7EXA57sMtBG0in1wrmm1w~lzUlYXaBPj6yO-uGN89DYcNJldHZM2jjj7BjzRO28jHPsoWnYD8jSjqFXzTwqr4ifTadZaAUcyjoS8cToB6uXzdy7~dhhUQnPHO9IyLHLCp7~B~0JfBMWvxs1NpsjidMKBitMebRO1kA-j0K6zQI3R~tN8PA7sOwMcBnj~wulErRq4Uk1s7ng0b0kKzj~8cBhrLUPBhNk~vEPLSMnNV0uG7r78yaaIlKspwInYxmT29mh~FUpt6SaPtmpaS2ABKGkhUQ__"
           alt="discount"
         />
       </div>
